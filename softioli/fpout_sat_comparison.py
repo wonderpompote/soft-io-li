b34adc9f71ec3dbaaa99c2cdf2c39b30cb093380
julie <julie.patuel@aero.obs-mip.fr>
2024-03-04 11:57:16 +0100

    fpout_sat_comparison quasi fini mannque fichier intermédiair)

--- softioli/fpout_sat_comparison.py
+++ softioli/fpout_sat_comparison.py
@@ -58,21 +58,20 @@ def get_fp_out_ds_7days(fpout_path, sum_height=True, load=False, chunks='auto', 
     :param assign_releases_position_coords:
     :return:
     """
-    # TODO: recup un dataset sur les infos de la release ET un dataArray spec001_mr ?
     if not utils.str_to_path(fpout_path).exists():
         raise ValueError(f'fp_path {fpout_path} does NOT exist')
     fp_ds = fpout.open_fp_dataset(fpout_path, chunks=chunks, max_chunk_size=max_chunk_size,
                                   assign_releases_position_coords=assign_releases_position_coords)\
-                .squeeze('nageclass')
+                    .squeeze('nageclass')
     # rename numpoint dimension to pointspec
     fp_ds = fp_ds.rename({'numpoint': 'pointspec'})
     # get dataset containing releases info (RELxxxx variables)
     rel_ds = fp_ds.drop_vars([var for var in fp_ds.variables if not 'REL' in var])
-    # fp simulation start date (ietime here because backwards)
+    # fp simulation "start" date (ietime here because backwards)
     ietime = pd.Timestamp(f"{fp_ds.attrs['iedate']}{fp_ds.attrs['ietime']}")
-    # fp release start dates (RELEND because backwards) --> get nearest hour before start
+    # fp release "start" dates (RELEND because backwards) --> get nearest hour before start
     release_start_dates = (ietime + fp_ds.RELEND).dt.ceil('h')
-    # get end date (release_start_date - 7 days)
+    # get "end" date (release_start_date - 7 days)
     end_dates = release_start_dates - np.timedelta64(7, 'D')
     # get spec001_mr over 7 days
     date_mask = (fp_ds.time >= end_dates) & (fp_ds.time <= release_start_dates)
@@ -86,33 +85,12 @@ def get_fp_out_ds_7days(fpout_path, sum_height=True, load=False, chunks='auto', 
         fp_ds.load()
     return fp_ds
 
-
-#TODO ça va servir à rien je pense
-def get_release_info_ds(fp_ds, chunks='auto', max_chunk_size=1e8, assign_releases_position_coords=False):
-    """
-    Returns dataset containing only release info (RELxxxx variables from FLEXPART output)
-    :param fp_ds: <xarray.Dataset> (or <pathlib.Path> or <str> pointing to existing FLEXPART output file)
-    :param chunks:
-    :param max_chunk_size:
-    :param assign_releases_position_coords:
-    :return: <xarray.Dataset>
-    """
-    if not isinstance(fp_ds, xr.Dataset):
-        if utils.str_to_path(fp_ds).exists():
-            fp_ds = fpout.open_dataset(url=fp_ds, chunks=chunks, max_chunk_size=max_chunk_size,
-                                  assign_releases_position_coords=assign_releases_position_coords)
-        else:
-            raise ValueError(f'fp_path {fp_ds} does NOT exist')
-    return fp_ds.drop_vars([var for var in fp_ds.variables if not 'REL' in var])
-
-
-
 # TODO: suppr dry_run une fois que les tests sont finis
 # TODO: pour avoir un sat_ds avec PLUSIEURS sources sat --> sat_name = list, for loop et ensuite je merge tout ?
 def get_satellite_ds(start_date, end_date, sat_name, grid_resolution=cts.GRID_RESOLUTION,
                      grid_res_str=cts.GRID_RESOLUTION_STR, overwrite=False, dry_run=False):
     """
-
+    Returns dataset with regridded satellite data between start and end date
     @param start_date:
     @param end_date:
     @param sat_name:
@@ -143,6 +121,7 @@ def get_satellite_ds(start_date, end_date, sat_name, grid_resolution=cts.GRID_RE
     }
     # check if missing_raw_daily_dir_list is empty, if not --> check if pre-regrid directories exist
     if missing_raw_daily_dir_list:
+        # TODO: suppr les prints de debug
         ########################################"
         print()
         print(f"regrid_daily_dir_list : {regrid_daily_dir_list}")
@@ -154,7 +133,7 @@ def get_satellite_ds(start_date, end_date, sat_name, grid_resolution=cts.GRID_RE
         dir_to_regrid_list = {d_path for d_path in missing_raw_daily_dir_list if d_path.exists()}
         # regrid the files in the missing directories
         if dir_to_regrid_list:
-            # TODO: tests on fait juste print for now
+            # TODO: suppr les prints de debug
             ##########################################
             print(f'Directories to regrid: {sorted(dir_to_regrid_list)}')
             print()
@@ -172,14 +151,17 @@ def get_satellite_ds(start_date, end_date, sat_name, grid_resolution=cts.GRID_RE
             raise FileNotFoundError(f'The GLM files for the following dates are missing: {sorted(missing_dates)}\nPlease download '
                                     f'them from the ICARE server and try again')
     # get list of satellite data files between start and end date
-    # TODO: PROBLEM !!!! CA PREND TOUTES LES HEURES, MAIS FAUT PAS CA POUR START ET END DAY
-    regrid_daily_file_list = get_sat_files_list_between_start_end_date(dir_list=sorted(regrid_daily_dir_list), start_date=start_date, end_date=end_date, sat_name=sat_name, regrid=True)
+    regrid_daily_file_list = get_sat_files_list_between_start_end_date(dir_list=sorted(regrid_daily_dir_list),
+                                                                       start_date=start_date, end_date=end_date,
+                                                                       sat_name=sat_name, regrid=True)
+    # TODO: suppr les prints de debug
     ##########################################
     print(f'Regrid daily file list: {short_list_repr(regrid_daily_file_list)}')
     print()
     ##########################################
     # create a dataset merging all the regrid hourly files
-    return xr.open_mfdataset(regrid_daily_file_list, combine_attrs='drop_conflicts')  #TODO: <?> utiliser dask: ajouter parallel=True
+    sat_ds = xr.open_mfdataset(regrid_daily_file_list, combine_attrs='drop_conflicts') #TODO: <?> utiliser dask: ajouter parallel=True
+    return sat_ds
 
 
 def get_weighted_flash_count(spec001_mr_da, flash_count_da):
@@ -191,12 +173,13 @@ def get_weighted_flash_count(spec001_mr_da, flash_count_da):
     """
     return (spec001_mr_da * flash_count_da).sum(['latitude', 'longitude']) / 3600
 
+
 #TODO: <!> prendre 7 days from RELSTART !! NOT min date en fait --> du coup recup closest heure AVANT relstart je suppose ?
-def get_weighted_fp_sat_ds(fp_da, sat_ds, sum_height=True, load=False, chunks='auto',
+def get_weighted_fp_sat_ds(fp_ds, sat_ds, sum_height=True, load=False, chunks='auto',
                            max_chunk_size=1e8, assign_releases_position_coords=False):
     """
 
-    @param fp_da: <xarray.DataArray> or <pathlib.Path> (or <str>) path to existing fp out netcdf file
+    @param fp_ds: <xarray.Dataset> or <pathlib.Path> (or <str>) path to existing fp out netcdf file
     @param sat_ds: <xarray.Dataset>
     @param sum_height:
     @param load:
@@ -206,27 +189,27 @@ def get_weighted_fp_sat_ds(fp_da, sat_ds, sum_height=True, load=False, chunks='a
     @return:
     """
     # if passed fp_out path instead of dataArray/dataset
-    if not isinstance(fp_da, xr.DataArray):
+    if not (isinstance(fp_ds, xr.DataArray) or isinstance(fp_ds, xr.Dataset)):
         # check fp_path and get fp_da
-        if utils.str_to_path(fp_da).exists():
-            fp_da = get_fp_out_ds_7days(fpout_path=fp_da, sum_height=sum_height, load=load,
+        if utils.str_to_path(fp_ds).exists():
+            fp_ds = get_fp_out_ds_7days(fpout_path=fp_ds, sum_height=sum_height, load=load,
                                         chunks=chunks, max_chunk_size=max_chunk_size,
                                         assign_releases_position_coords=assign_releases_position_coords)
         else:
             raise TypeError(
-            f'Invalid fp_da ({fp_da}). Expecting <xarray.Dataset> or path (<str> or <pathlib.Path>) to existing FLEXPART output file')
+                f'Invalid fp_da ({fp_ds}). Expecting <xarray.Dataset> or path (<str> or <pathlib.Path>) to existing FLEXPART output file')
     if not isinstance(sat_ds, xr.Dataset):
         raise TypeError(
             f'Invalid sat_ds ({sat_ds}). Expecting <xarray.Dataset> object')
     # merge fp da and sat ds
-    fp_sat_ds = xr.merge([fp_da, sat_ds])
+    fp_sat_ds = xr.merge([fp_ds, sat_ds.flash_count], combine_attrs='drop_conflicts')
     # get weighted flash count
     fp_sat_ds['weighted_flash_count'] = get_weighted_flash_count(spec001_mr_da=fp_sat_ds['spec001_mr'],
                                                                  flash_count_da=fp_sat_ds['flash_count'])
     return fp_sat_ds
 
-# TODO: fp_sat_comp function qui prend juste fp_path (list or not) as input and computes the weighted flash_count (and fichier intermédiaure etc., à voir)
-# TODO: fp_sat_comp doit savoir TOUT SEUL quelles données sat on va chercher en fonction de ce qui est dispo et tout
+
+# TODO: fp_sat_comp doit savoir TOUT SEUL quelles données sat on va chercher en fonction de ce qui est dispo et tout (? pourquoi j'ai dit ça?)
 def fpout_sat_comparison(fp_path, sat_name, file_list=False, sum_height=True, load=False, chunks='auto',
                          max_chunk_size=1e8, assign_releases_position_coords=False, grid_resolution=cts.GRID_RESOLUTION,
                          grid_res_str=cts.GRID_RESOLUTION_STR):
@@ -237,16 +220,15 @@ def fpout_sat_comparison(fp_path, sat_name, file_list=False, sum_height=True, lo
         if not utils.str_to_path(fp_file).exists():
             raise FileNotFoundError(f'Expecting existing fp out file! {fp_file} does NOT exist')
         # step2: recup fp_ds sur 7 JOURS avec les 7j pour chaque releases, PAS depuis début fichier
-        fp_ds = get_fp_out_ds_7days(fpout_path=fp_file, sum_height=sum_height, load=load, chunks=chunks, max_chunk_size=max_chunk_size,
-                                    assign_releases_position_coords=assign_releases_position_coords)
-        # TODO: step3: recup liste des sat_name des zones couvertes
-        # step4: recup start + end date
-        start_date, end_date = pd.Timestamp(fp_ds.time.min().values), pd.Timestamp(fp_ds.time.max())
-        #   step4: get sat_ds
-        sat_ds = get_satellite_ds(start_date=start_date, end_date=end_date, sat_name=sat_name, grid_resolution=grid_resolution,
-                                  grid_res_str=grid_res_str)
-        # setp5: get weighted fp_sat_ds
-        weighted_fp_sat_ds = get_weighted_fp_sat_ds(fp_da=fp_ds.spec001_mr, sat_ds=sat_ds)
-        # step6: générer le fichier intermédiaire
-        # --> je crois si je fais juste la somme de flash_count_weighted sur toutes les dims sauf pointspec c'est ok non ?
-        # TODO: tester fpout_sat_comparison pour voir si get weighted OK
+        with get_fp_out_ds_7days(fpout_path=fp_file, sum_height=sum_height, load=load, chunks=chunks,
+                                 max_chunk_size=max_chunk_size, assign_releases_position_coords=assign_releases_position_coords)\
+                as fp_ds:
+            # TODO: step3: recup liste des sat_name des zones couvertes
+            start_date, end_date = pd.Timestamp(fp_ds.time.min().values), pd.Timestamp(fp_ds.time.max().values)
+            #   step4: get sat_ds
+            sat_ds = get_satellite_ds(start_date=start_date, end_date=end_date, sat_name=sat_name, grid_resolution=grid_resolution,
+                                      grid_res_str=grid_res_str)
+            # setp5: get weighted fp_sat_ds
+            weighted_fp_sat_ds = get_weighted_fp_sat_ds(fp_ds=fp_ds, sat_ds=sat_ds.flash_count)
+            # TODO: step6: générer le fichier intermédiaire <!>
+            # TODO: pour chaque RELSTART donner weighted_fp_sat_ds['weighted_flash_count'].sum('time')
