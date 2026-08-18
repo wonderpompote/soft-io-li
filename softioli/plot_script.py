87057e05f7a346e3d796015c7c567015e534325b
julie <julie.patuel@aero.obs-mip.fr>
2026-08-18 17:14:07 +0200

    (fix) indent issue with plot_script.py

--- softioli/plot_script.py
+++ softioli/plot_script.py
@@ -136,77 +136,77 @@ if __name__ == "__main__":
     # plot parameters
     plot_group = parser.add_argument_group("Plot parameters")
 
-plot_group.add_argument('-x', '--x-axis-var', default='longitude',
-                        help='x-axis variable name (default=longitude)')
-plot_group.add_argument('--xlabel', default='Longitude (°)',
-                        help='x-axis label (default=Longitude (°))')
-plot_group.add_argument('-y', '--y-axis-var', default='latitude',
-                        help='y-axis  variable name (default=latitude)')
-plot_group.add_argument('--ylabel', default='Latitude (°)',
-                        help='y-axis label (default=Latitude (°))')
-plot_group.add_argument('-c', '--c-var', default='spec001_mr',
-                        help='c variable name (default=spec001_mr)')
-plot_group.add_argument('--clabel', default='residence time (s)',
-                        help='clabel (default=residence time (s))')
-plot_group.add_argument('--title', help='Plot title')
-plot_group.add_argument('--cmap', default='coolwarm',
-                        help='color map (default=coolwarm)')
-plot_group.add_argument('--log-scale', action='store_true',
-                        help='Indicates if log scale should be used')
+    plot_group.add_argument('-x', '--x-axis-var', default='longitude',
+                            help='x-axis variable name (default=longitude)')
+    plot_group.add_argument('--xlabel', default='Longitude (°)',
+                            help='x-axis label (default=Longitude (°))')
+    plot_group.add_argument('-y', '--y-axis-var', default='latitude',
+                            help='y-axis  variable name (default=latitude)')
+    plot_group.add_argument('--ylabel', default='Latitude (°)',
+                            help='y-axis label (default=Latitude (°))')
+    plot_group.add_argument('-c', '--c-var', default='spec001_mr',
+                            help='c variable name (default=spec001_mr)')
+    plot_group.add_argument('--clabel', default='residence time (s)',
+                            help='clabel (default=residence time (s))')
+    plot_group.add_argument('--title', help='Plot title')
+    plot_group.add_argument('--cmap', default='coolwarm',
+                            help='color map (default=coolwarm)')
+    plot_group.add_argument('--log-scale', action='store_true',
+                            help='Indicates if log scale should be used')
 
-args = parser.parse_args()
-print(args)
+    args = parser.parse_args()
+    print(args)
 
-# retrieve list of flight ids
-flight_id_list = []
-if args.flight_id_list:  # txt file containing flight ids
-    flight_id_list = list_from_file(args.flight_id_list, header=0, ignore_blank_lines=True)
-else:  # flight ids passed directly in command line
-    flight_id_list = args.flight_id
+    # retrieve list of flight ids
+    flight_id_list = []
+    if args.flight_id_list:  # txt file containing flight ids
+        flight_id_list = list_from_file(args.flight_id_list, header=0, ignore_blank_lines=True)
+    else:  # flight ids passed directly in command line
+        flight_id_list = args.flight_id
 
-# create output dir if it doesn't exist
-pathlib.Path(args.output_dir).mkdir(exist_ok=True, parents=True)
+    # create output dir if it doesn't exist
+    pathlib.Path(args.output_dir).mkdir(exist_ok=True, parents=True)
 
-for flight_id in flight_id_list:
-    # plot residence time over longitude - latitude (summed over height and time)
-    if args.fp_summed_res_time:
-        fpout_dirpath = f'{args.softioli_output_dir}/{flight_id}/{args.fp_output_dirname}'
-        if not pathlib.Path(fpout_dirpath).exists():
-            print(f'{fpout_dirpath} does not exist ! Skipping flight {flight_id}')
-            continue
+    for flight_id in flight_id_list:
+        # plot residence time over longitude - latitude (summed over height and time)
+        if args.fp_summed_res_time:
+            fpout_dirpath = f'{args.softioli_output_dir}/{flight_id}/{args.fp_output_dirname}'
+            if not pathlib.Path(fpout_dirpath).exists():
+                print(f'{fpout_dirpath} does not exist ! Skipping flight {flight_id}')
+                continue
+            else:
+                fpout_nc_filepath = get_fpout_nc_file_path_from_fp_dir(fp_dirpath=fpout_dirpath)
+                fp_da = get_fp_out_ds_xdays(fpout_nc_filepath, days=7, sum_height=True, chunks='auto',
+                                            max_chunk_size=1e8, assign_releases_position_coords=False)['spec001_mr']
+                for rel_index in fp_da.pointspec.data:
+                    # check if plot already exists (or should be overwritten)
+                    rel_index_str = str(rel_index)
+                    plot_filename = args.plot_filename if args.plot_filename else f'{flight_id}_rel{rel_index_str}_res-time-summed-over-7days.png'
+                    if not pathlib.Path(f'{args.output_dir}/{plot_filename}').exists() or args.overwrite:
+                        fp_da_relx = fp_da.isel(pointspec=rel_index).sum('time')
+                        plot_title = args.title if args.title else f'Flight {flight_id} rel{rel_index_str} - residence time summed over 7 days'
+
+                        plot_pcolor_da(
+                            x=fp_da_relx[args.x_axis_var], xlabel=args.xlabel,
+                            y=fp_da_relx[args.y_axis_var], ylabel=args.ylabel,
+                            c=fp_da_relx, clabel=args.clabel,
+                            title=plot_title,
+                            xlim=None, ylim=None, clim=None,
+                            cmap=args.cmap, log_scale=args.log_scale,
+                            display_coastlines=True, central_lon=None,
+                            save_plot=True, overwrite=args.overwrite,
+                            res_dirpath=args.output_dir, res_filename=plot_filename,
+                            figsize=(12, 6), show_plot=False
+                        )
+                    else:
+                        print(
+                            f'{args.output_dir}/{plot_filename} already exists! Use --overwrite option if you want to overwrite it. Skipping flight {flight_id}')
+
+        elif args.fp_hourly_res_time:
+            print('Hourly flexpart residence time plot not implemented yet!')
+
         else:
-            fpout_nc_filepath = get_fpout_nc_file_path_from_fp_dir(fp_dirpath=fpout_dirpath)
-            fp_da = get_fp_out_ds_xdays(fpout_nc_filepath, days=7, sum_height=True, chunks='auto',
-                                        max_chunk_size=1e8, assign_releases_position_coords=False)['spec001_mr']
-            for rel_index in fp_da.pointspec.data:
-                # check if plot already exists (or should be overwritten)
-                rel_index_str = str(rel_index)
-                plot_filename = args.plot_filename if args.plot_filename else f'{flight_id}_rel{rel_index_str}_res-time-summed-over-7days.png'
-                if not pathlib.Path(f'{args.output_dir}/{plot_filename}').exists() or args.overwrite:
-                    fp_da_relx = fp_da.isel(pointspec=rel_index).sum('time')
-                    plot_title = args.title if args.title else f'Flight {flight_id} rel{rel_index_str} - residence time summed over 7 days'
+            print(f'No other plot type supported for now')
 
-                    plot_pcolor_da(
-                        x=fp_da_relx[args.x_axis_var], xlabel=args.xlabel,
-                        y=fp_da_relx[args.y_axis_var], ylabel=args.ylabel,
-                        c=fp_da_relx, clabel=args.clabel,
-                        title=plot_title,
-                        xlim=None, ylim=None, clim=None,
-                        cmap=args.cmap, log_scale=args.log_scale,
-                        display_coastlines=True, central_lon=None,
-                        save_plot=True, overwrite=args.overwrite,
-                        res_dirpath=args.output_dir, res_filename=plot_filename,
-                        figsize=(12, 6), show_plot=False
-                    )
-                else:
-                    print(
-                        f'{args.output_dir}/{plot_filename} already exists! Use --overwrite option if you want to overwrite it. Skipping flight {flight_id}')
-
-    elif args.fp_hourly_res_time:
-        print('Hourly flexpart residence time plot not implemented yet!')
-
-    else:
-        print(f'No other plot type supported for now')
-
-print('---\n')
+    print('---\n')
 
